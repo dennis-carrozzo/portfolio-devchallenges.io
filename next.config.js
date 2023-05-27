2b7a4764add8b39b4f56461c448d044156feaef3
denniswebdel <denniswebdel@gmail.com>
2023-05-27 20:02:17 +0100

    update next.config.js
    add whitelisted domain for images and env varaibles mapping

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
+  env: {
+    storyblokPreviewToken: process.env.STORYBLOK_PREVIEW_TOKEN
+  },
+  images: {
+    domains: ['a.storyblok.com']
+  }
 }
 
 module.exports = nextConfig
