a26f1c3a3dbfa80c01dc372ba071197273c9a5da
denniswebdel <denniswebdel@gmail.com>
2023-06-03 09:45:59 +0100

    feat(contact): add API route for contact form, validate form and send email
    Added a new handler function for the contact form which returns an appropriate response code based on the method and body of the request. The handler function validates the request body and sends an email to the admin and the user. The validateContactForm util function was also added to validate the form fields. The email is sent through the Sendgrid mail API. The commit also includes some 'todo' comments for future improvements.

---
+++ src/pages/api/contact.js
@@ -0,0 +1,72 @@
+import validateContactForm from '@/utils/validateContactForm'
+import sgMail from '@sendgrid/mail'
+
+// todo: test security with postman
+// todo: add logging handling to 3rd party provider
+// todo: add confirmation email
+export default async function handler ({ method, body }, res) {
+  // validating request method
+  if (method !== 'POST') {
+    res.status(405).json({ message: 'Invalid Method' })
+  }
+  // validating request body
+  const errors = validateContactForm(body)
+  if (Object.keys(errors).length > 0) {
+    res.status(400).json({ message: 'Invalid body' })
+  }
+  // emailing admin
+  try {
+    const response = await emailAdmin(body)
+    if (!response) {
+      throw new Error('error sending message')
+    }
+    const responseUser = await emailUser(body)
+    if (!responseUser) {
+      throw new Error('error sending message')
+    }
+  } catch (e) {
+    return res.status(500).json({ message: 'Something Went Wrong' })
+  }
+  res.status(200).json({ message: 'successful' })
+}
+
+async function emailAdmin (body) {
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+  const msg = {
+    to: process.env.ADMIN_EMAIL,
+    from: process.env.ADMIN_EMAIL,
+    subject: `portfolio contact form message from ${body.name}`,
+    text: `${body.message}\n\t${body.email}`,
+    html: `${body.message}\n\t${body.email}`
+  }
+  try {
+    const response = await sgMail.send(msg)
+    if (!response[0]?.statusCode?.toString().match(/^2\d\d$/)) {
+      throw new Error('sendgrid error')
+    }
+    return true
+  } catch (error) {
+    console.error('error', error)
+    return false
+  }
+}
+async function emailUser (body) {
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+  const msg = {
+    to: body.email,
+    from: process.env.ADMIN_EMAIL,
+    subject: `Thank you for contacting`,
+    text: 'thanks',
+    body: 'thanks'
+  }
+  try {
+    const response = await sgMail.send(msg)
+    if (!response[0]?.statusCode?.toString().match(/^2\d\d$/)) {
+      throw new Error('sendgrid error')
+    }
+    return true
+  } catch (error) {
+    console.error('error', error)
+    return false
+  }
+}
