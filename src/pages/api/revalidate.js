3022c5d3341a3b35f1ba12ff507db41b666961d0
denniswebdel <denniswebdel@gmail.com>
2023-06-09 18:38:58 +0100

    feat(cache):  implement on demand cache invalidation
    Export function `getStaticProps` no longer includes a `revalidate` key. Instead, route `/api/revalidate` performs cache invalidation operations when CMS webhook is triggered. Secrets are stored in environment variables and signatures are verified for security.

---
+++ src/pages/api/revalidate.js
@@ -0,0 +1,25 @@
+import crypto from 'node:crypto'
+
+export default async function handler (req, res) {
+  try {
+    verifySignature(req.body, req.headers['webhook-signature'])
+    const pathToRevalidate =
+      req.body.full_slug === 'home' ? '/' : `/${req.body.full_slug}`
+    await res.revalidate(pathToRevalidate)
+    return res.status(200).json({ revalidated: true })
+  } catch (err) {
+    return res.status(500).json({ message: 'Error revalidating' })
+  }
+}
+
+function verifySignature (body, signature) {
+  const webhookSecret = process.env.CACHE_INVALIDATION_TOKEN
+  let bodyHmac = crypto
+    .createHmac('sha1', webhookSecret)
+    .update(JSON.stringify(body))
+    .digest('hex')
+
+  if (bodyHmac !== signature) {
+    throw new Error('Signature mismatch!')
+  }
+}
